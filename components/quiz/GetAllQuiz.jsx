9654789c4add11d502ddf222c8d8f8afe2f151d2
AnaMarija209 <82404148+AnaMarija209@users.noreply.github.com>
2024-07-05 20:27:29 +0200

    Update GetAllQuiz.jsx

--- components/quiz/GetAllQuiz.jsx
+++ components/quiz/GetAllQuiz.jsx
@@ -1,83 +1,87 @@
-import React, {useEffect, useState} from 'react'
-import {getAllQuestions} from "../../utils/QuizService.jsx";
+import React, { useEffect, useState } from "react"
+import { deleteQuestion, getAllQuestions } from "../../../utils/QuizService"
+import { Link } from "react-router-dom"
+import {FaPlus} from "react-icons/fa"
 
 const GetAllQuiz = () => {
-    const [question, setQuestions] = useState([{id: "",
-        question: "",
-        correctAnswers: "",
-        choices: []}])
-    const [isLoading, setIsLoading] = useState(true)
-    const [isQuestionDeleted, setIsQuestionDeleted] = useState(false)
-    const [deleteSuccessMessage, setDeleteSuccessMessage] = useState("")
+	const [questions, setQuestions] = useState([
+		{ id: "", question: "", correctAnswers: "", choices: [] }
+	])
+	const [isLoading, setIsLoading] = useState(true)
+	const [isQuestionDeleted, setIsQuestionDeleted] = useState(false)
+	const [deleteSuccess, setDeleteSuccess] = useState("")
 
-    useEffect(() => {
-        fetchAllQuestions()
-    }, [])
-    const fetchAllQuestions = async () => {
-        try {
-            const data = await getAllQuestions()
-            setQuestions(data)
-            setIsLoading(false)
-        } catch (error) {
-            console.error(error)
-        }
-    }
+	useEffect(() => {
+		fetchQuestions()
+	}, [])
 
-    const handleDelete = async (id) => {
-        try {
-            await deleteQuestion(id)
-            setQuestions(questions.filter((question) => question.id !== id))
-            setIsQuestionDeleted(true)
-            setDeleteSuccessMessage("Question deleted successfully.")
-        } catch (error) {
-            console.error(error)
-        }
-        setTimeout(() => {
-            setDeleteSuccessMessage("")
-        }, 4000)
-    }
+	const fetchQuestions = async () => {
+		try {
+			const data = await getAllQuestions()
+			setQuestions(data)
+			setIsLoading(false)
+		} catch (error) {
+			console.error(error)
+		}
+	}
 
-    if(isLoading){
-        return <div>Loading...</div>
-    }
-        return (
-            <section className={"container"}>
-                <div className={"row mt-5"}>
-                    <div className={"col-md-6 mb-2 md-mb-0"}
-                    style={{color:"GrayText"}}>
-                        <h4> All Quiz Questions</h4>
-                    </div>
-                    <div className={"col-md-4 d-flex justify-content-end"}>
-                        {/* TODO: add a link to navigate to add new question form */}
-                    </div>
-                </div>
-                <hr/>
-                {isQuestionDeleted && <div className={"alert alert-succes"}>{deleteSuccessMessage}</div>}
+	const handleDeleteQuestion = async (id) => {
+		try {
+			await deleteQuestion(id)
+			setQuestions(questions.filter((question) => question.id !== id))
+			setIsQuestionDeleted(true)
+			setDeleteSuccess("Question deleted successfully.")
+		} catch (error) {
+			console.error(error)
+		}
+		setTimeout(() => {
+			setDeleteSuccess("")
+		}, 4000)
+	}
 
-                {question.map((question, index) =>(
-                    <div>
-                        <h4 style={{color:"GrayText"}}>{'$(index + 1)}. ${question.question}'}</h4>
-                        <ul>
-                            {question.choices.map((choice, index) => (
-                                <li key={index}>
-                                    {choice}
-                                </li>
-                            ))}
-                        </ul>
-                        <p className={"text-success"}>Correct Answer: {question.correctAnswers}</p>
-                        <div className={"btn-group mb-4"}>
-                            {/* TODO: add a link to navigate to update question form */}
+	if (isLoading) {
+		return <p>Loading...</p>
+	}
 
-                            <button
-                                className={"btn btn-outline-danger btn-sm"}
-                                onClick={() => (handleDelete(question.id))}>
-                                Delete Question
-                            </button>
-                        </div>
-                    </div>
-                ))}
-            </section>
-        )
+	return (
+		<section className="container">
+			<div className="row mt-5">
+				<div className="col-md-6 mb-2 md-mb-0" style={{ color: "GrayText" }}>
+					<h4>All Quiz Questions</h4>
+				</div>
+				<div className="col-md-4 d-flex justify-content-end">
+					<Link to={"/create-quiz"}>
+						<FaPlus /> Add Question
+					</Link>
+				</div>
+			</div>
+			<hr />
+			{isQuestionDeleted && <div className="alert alert-success">{deleteSuccess}</div>}
+			{questions.map((question, index) => (
+				<div key={question.id}>
+					<pre>
+						<h4 style={{ color: "GrayText" }}>{`${index + 1}. ${question.question}`}</h4>
+					</pre>
+					<ul>
+						{question.choices.map((choice, index) => (
+							<li key={index}>{choice}</li>
+						))}
+					</ul>
+					<p className="text-success">Correct Answer: {question.correctAnswers}</p>
+					<div className="btn-group mb-4">
+						<Link to={`/update-quiz/${question.id}`}>
+							<button className="btn btn-sm btn-outline-warning mr-2">Edit Question</button>
+						</Link>
+						<button
+							className="btn btn-sm btn-outline-danger"
+							onClick={() => handleDeleteQuestion(question.id)}>
+							Delete Question
+						</button>
+					</div>
+				</div>
+			))}
+		</section>
+	)
 }
 
 export default GetAllQuiz
